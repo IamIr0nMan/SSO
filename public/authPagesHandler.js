a827196be3f93e96b4c7c58a1b4cf6b3f826016b
Deepak Kumar <iamdpk18@gmail.com>
2023-03-17 03:46:52 +0530

    Add on click function to register users

--- public/authPagesHandler.js
+++ public/authPagesHandler.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 function redirectToRegister() {
   location.href = "/register";
 }
@@ -47,10 +49,50 @@ function signIn() {
       }
     })
     .catch((error) => {
-      console.error("Error:", error);
+      console.error("Error: ", error);
     });
 }
 
-function register() {}
+function register() {
+  const fname = document.getElementById("floatingFirstName").value;
+  const lname = document.getElementById("floatingLastName").value;
+  const email = document.getElementById("floatingEmail").value;
+  const password = document.getElementById("floatingPassword").value;
+  const dob = document.getElementById("datepicker").value;
+  const errorMsg = document.getElementById("errorMsg");
+  const data = { fname, lname, email, password, dob };
+
+  fetch("http://localhost:3000/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.status == 422) {
+        errorMsg.innerText =
+          "This user already exist, redirecting to sign in page...";
+        setTimeout(() => {
+          location.href = "/login";
+        }, 3000);
+      } else if (response.status != 200) {
+        errorMsg.innerText = "There is an unexpected error, try again later...";
+        setTimeout(() => {
+          location.reload();
+        }, 3000);
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      if (response.status == 200) {
+        location.href = data.redirectionURL;
+      }
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+    });
+}
 
 function forgotPassword() {}
