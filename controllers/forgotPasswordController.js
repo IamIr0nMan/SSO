6050725aaf936df7688c10eaed87d5560e894a4b
Deepak Kumar <iamdpk18@gmail.com>
2023-03-17 04:13:48 +0530

    Add forgot password controller and its caller funtion on frontend

--- controllers/forgotPasswordController.js
+++ controllers/forgotPasswordController.js
@@ -1,3 +1,55 @@
-const forgotPassword = async (req, res, next) => {};
+const bcrypt = require("bcrypt");
+
+const HttpError = require("../models/httpError");
+const User = require("../models/userSchema");
+
+const forgotPassword = async (req, res, next) => {
+  const { email, newPassword, dob } = req.body;
+  let user;
+
+  try {
+    user = await User.findOne({ email: email });
+  } catch (error) {
+    console.error("Error checking existing user on password reset");
+    console.log(error);
+    return next(new HttpError("Password reset failed, please try again", 500));
+  }
+
+  if (!user)
+    return next(new HttpError("User does not exist, please sign up", 404));
+
+  if (user.dob != new Date(dob))
+    return next(
+      new HttpError(
+        "Invalid date of birth, can't reset password, please try again",
+        401
+      )
+    );
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(newPassword, 12);
+  } catch (error) {
+    console.error("Error hashing the password when resetting password");
+    console.log(error);
+    return next(
+      new HttpError("Password reset failed, please try again later", 500)
+    );
+  }
+
+  user.password = hashedPassword;
+
+  try {
+    user = await user.save();
+  } catch (error) {
+    console.error("Error updating password of user to database");
+    console.log(error);
+    return next(
+      new HttpError("Password reset failed, please try again later", 500)
+    );
+  }
+
+  res.status(200).json({ message: "Password reset successful" });
+};
 
 module.exports = forgotPassword;
--- public/authPagesHandler.js
+++ public/authPagesHandler.js
@@ -95,4 +95,43 @@ function register() {
     });
 }
 
-function forgotPassword() {}
+function forgotPassword() {
+  const email = document.getElementById("floatingEmail").value;
+  const newPassword = document.getElementById("floatingPassword").value;
+  const dob = document.getElementById("datepicker").value;
+  const errorMsg = document.getElementById("errorMsg");
+  const data = { email, newPassword, dob };
+
+  fetch("http://localhost:3000/auth/forgotPassword", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.status == 404) {
+        errorMsg.innerText =
+          "This user does not exist, redirecting to signup...";
+        setTimeout(() => {
+          location.href = "/register";
+        }, 3000);
+      } else if (response.status == 401) {
+        errorMsg.innerText = "Date of Birth is incorrect, try again...";
+      } else if (response.status != 200) {
+        errorMsg.innerText = "There is an unexpected error, try again later...";
+        setTimeout(() => {
+          location.reload();
+        }, 3000);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (response.status == 200) {
+        location.href = "/login";
+      }
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+    });
+}
