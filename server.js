5fe60965f0fe06f57e1c922e7539b7a283cecbd2
Deepak Kumar <iamdpk18@gmail.com>
2023-03-16 16:03:06 +0530

    Create custom error class and error handler route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv";
 
+import HttpError from "./models/httpError";
+
 const app = express();
 
 dotenv.config();
@@ -9,6 +11,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  return new HttpError("Could not find this route...", 404);
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) return next(error);
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured..." });
+});
 
 app.listen(PORT, () => {
   console.log(`Access it on https://localhost:${PORT}/`);
