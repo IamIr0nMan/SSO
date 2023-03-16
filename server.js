faf22314b097fa093dab91c822c3c0a2dc84fcf6
Deepak Kumar <iamdpk18@gmail.com>
2023-03-16 16:09:00 +0530

    Add session and session store

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import { MongoDBStore } from "connect-mongodb-session";
+import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 
 import HttpError from "./models/httpError";
@@ -8,6 +12,34 @@ const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URL,
+  collection: process.env.DATABASE_NAME,
+  ttl: 60 * 60 * 24 * 10,
+  autoRemove: "native",
+});
+
+store.on("error", (error) => {
+  console.log(error);
+});
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true, httpOnly: true },
+    store: store,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
