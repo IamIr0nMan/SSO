feda70f3501c0e74075a5121cffb7c3a97bcb7b6
Deepak Kumar <iamdpk18@gmail.com>
2023-04-11 22:32:03 +0530

    Update logout controller method

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,24 +1,37 @@
-const HttpError = require("../models/httpError");
+const jwt = require("jsonwebtoken");
 
-function logout(store) {
-  return async function (req, res, next) {
-    const sessionId = req.session.id;
-    store.destroy(sessionId, (err) => {
-      if (err) {
-        console.error("Error deleting session data from database");
-        console.log(err);
-        return next(new HttpError("Problem logging out user", 500));
-      }
-      req.session.destroy((err) => {
-        if (err) {
-          console.error("Error deleting session data from req object");
-          console.log(err);
-          return next(new HttpError("Problem logging out user", 500));
-        }
-      });
-    });
-    res.status(200).json({ message: "User logged out successfully" });
+const HttpError = require("../models/httpError");
+const User = require("../models/userSchema");
+
+const logout = async (req, res, next) => {
+  const userId = req.body.userId;
+
+  let user;
+  try {
+    user = User.findById(userId);
+  } catch (error) {
+    console.log("Error logging out user");
+    console.error(error);
+    return next(new HttpError("Logout failed, please try again", 500));
+  }
+
+  if (!user)
+    return next(new HttpError("User does not exist, please sign up", 404));
+
+  user.tokens = {
+    accessToken: null,
+    refreshToken: null,
   };
-}
+
+  try {
+    await user.save();
+  } catch (error) {
+    console.error("Error saving tokens to database on logout");
+    console.log(error);
+    return next(new HttpError("Logout failed, please try again", 500));
+  }
+
+  res.status(200).json({ message: "User logged out successfully" });
+};
 
 module.exports = logout;
